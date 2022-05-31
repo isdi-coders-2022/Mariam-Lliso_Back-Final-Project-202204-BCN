33e15b4820def9c4d8af4f9fe357861904bcfc70
MariamLliso <contacto.mariamlliso@gmail.com>
2022-05-31 10:28:27 +0200

    created connection to database method

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
 const debug = require("debug")("vlcSinGluten:root");
 const chalk = require("chalk");
+const connectionToDatabase = require("./database");
 const initServer = require("./server/initServer");
+
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+const database = process.env.MONGO_DATABASE;
 
 const port = process.env.PORT ?? 4000;
 
 (async () => {
   try {
+    await connectionToDatabase({ username, password, database });
     await initServer(port);
   } catch {
     debug(chalk.red("Exiting with errors"));
